806aaad2e373170e523eabd517f9f86613f3c81f
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-05-06 16:21:16 +0700

    Add createbuilding function

--- createVertexBuilding.py
+++ createVertexBuilding.py
@@ -1,5 +1,5 @@
-vertex = []
-def drawBuilding(upperbackleft_x, upperbackleft_y, upperbackleft_z, upperbackright_x, upperbackright_y, upperbackright_z, upperfrontleft_x, upperfrontleft_y, upperfrontleft_z, upperfrontright_x, upperfrontright_y, upperfrontright_z, underbackleft_x, underbackleft_y, underbackleft_z, underbackright_x, underbackright_y, underbackright_z, underfrontleft_x, underfrontleft_y, underfrontleft_z, underfrontright_x, underfrontright_y, underfrontright_z):
+def createVertex(upperbackleft_x, upperbackleft_y, upperbackleft_z, upperbackright_x, upperbackright_y, upperbackright_z, upperfrontleft_x, upperfrontleft_y, upperfrontleft_z, upperfrontright_x, upperfrontright_y, upperfrontright_z, underbackleft_x, underbackleft_y, underbackleft_z, underbackright_x, underbackright_y, underbackright_z, underfrontleft_x, underfrontleft_y, underfrontleft_z, underfrontright_x, underfrontright_y, underfrontright_z):
+	vertex = []
 	# Left Side
 	vertex.append(upperfrontleft_x)
 	vertex.append(upperfrontleft_y)
@@ -52,7 +52,7 @@ def drawBuilding(upperbackleft_x, upperbackleft_y, upperbackleft_z, upperbackrig
 	vertex.append(underfrontright_x)
 	vertex.append(underfrontright_y)
 	vertex.append(underfrontright_z)
-	
+
 	vertex.append(upperbackright_x)
 	vertex.append(upperbackright_y)
 	vertex.append(upperbackright_z)
@@ -125,3 +125,25 @@ def drawBuilding(upperbackleft_x, upperbackleft_y, upperbackleft_z, upperbackrig
 	vertex.append(underfrontleft_x)
 	vertex.append(underfrontleft_y)
 	vertex.append(underfrontleft_z)
+
+	return vertex
+
+def createBuilding(underbackleft_x, underbackleft_y, underbackleft_z, underbackright_x, underbackright_y, underbackright_z, underfrontleft_x, underfrontleft_y, underfrontleft_z, underfrontright_x, underfrontright_y, underfrontright_z)
+	tinggi = 5
+	upperbackleft_x = underbackleft_x[2] + tinggi
+	upperbackleft_y = underbackleft_y[2] + tinggi
+	upperbackleft_z = underbackleft_z[2] + tinggi
+
+	upperbackright_x = underbackright_x[2] + tinggi
+	upperbackright_y = underbackright_y[2] + tinggi
+	upperbackright_z = underbackright_z[2] + tinggi
+
+	upperfrontright_x = underfrontright_x[2] + tinggi
+	upperfrontright_y = underfrontright_y[2] + tinggi
+	upperfrontright_z = underfrontright_z[2] + tinggi
+
+	upperfrontleft_x = underfront_left_x[2] + tinggi
+	upperfrontleft_y = underfront_left_y[2] + tinggi
+	upperfrontleft_z = underfront_left_z[2] + tinggi
+
+	return createVertex(upperbackleft_x, upperbackleft_y, upperbackleft_z, upperbackright_x, upperbackright_y, upperbackright_z, upperfrontleft_x, upperfrontleft_y, upperfrontleft_z, upperfrontright_x, upperfrontright_y, upperfrontright_z, underbackleft_x, underbackleft_y, underbackleft_z, underbackright_x, underbackright_y, underbackright_z, underfrontleft_x, underfrontleft_y, underfrontleft_z, underfrontright_x, underfrontright_y, underfrontright_z)
